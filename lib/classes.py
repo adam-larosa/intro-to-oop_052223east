f3b21d2e782fe47d654b676d54935900c1929ecf
Adam La Rosa <adam.larosa@flatironschool.com>
2023-07-05 08:53:14 -0700

    fin

--- lib/classes.py
+++ lib/classes.py
@@ -1,2 +1,69 @@
+
+# A "class" is nothing more than a "blueprint" to create "instances", which are
+# one of the things the blueprint is trying to make
+
+# So a "class" creates "instances", also called "objects"
+
 class Cat:
-    pass
+    # Cat.all will be accessable to ALL the instances
+    all = []
+
+    # this special method runs once every time an instance is created
+    def __init__( self, name_given, age_given, owner_instance ):
+        self.owner = owner_instance
+        self.name = name_given
+        self._age = age_given
+        # every time an instance is made, we can append a reference to it 
+        # in the "all" list
+        Cat.all.append( self )
+
+    def get_age( self ):
+        return self._age
+
+    def set_age( self, new_age ):
+        if new_age > 30:
+            print( "cat's don't live that long!" )
+        else:
+            self._age = new_age
+    
+    age = property( get_age, set_age )
+
+
+    # an instance method, a method we can call on an instance!
+    # e.g. c1.meow()
+    def meow( self ):
+        print( f'I am {self.name}, mee-yaow' )
+
+
+    def __repr__( self ):
+        return f'<Cat name="{self.name}" age={self.age}>'
+
+
+class Owner:
+    def __init__( self, name ):
+        self.name = name
+
+    @property
+    def cats( self ):
+        # cat_list = []
+        # for cat_instance in Cat.all:
+        #     if cat_instance.owner == self:
+        #         cat_list.append( cat_instance )
+        # return cat_list
+
+
+#          this is what end up
+#            in the new list
+#                 |
+#                 v    
+        return [ cat for cat in Cat.all if cat.owner == self ]
+#                         ^
+#                         |
+#                    a variable we 
+#                  are declaring to 
+#                 represent on of the
+#                   cats in Cat.all
+
+
+    def __repr__( self ):
+        return f'<Owner name="{self.name}">'
--- run.py
+++ run.py
@@ -1,5 +1,14 @@
 import ipdb
-from lib.classes import Cat
+import random
+from lib.classes import Cat, Owner
 
+o1 = Owner( 'adam' )
+o2 = Owner( 'ix' )
 
+c1 = Cat( 'Luke', 5, o1 )
+c2 = Cat( 'Murphy', 3, o1 )
+c3 = Cat( 'Rose', 12, o2 )
+
+# to exit, we "continue" the debugger by typing "c" then hitting enter
 ipdb.set_trace()
+print( 'bye bye!' )
